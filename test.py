4b3f7e6aa46b73da69e174cf68cc9713ac7f8e3d
Willianwt2000 <willianmetelien@gmail.com>
2026-03-23 23:23:05 -0400

    fix

--- test.py
+++ test.py
@@ -103,3 +103,25 @@ print(type(str(employee_age)))
 # # Add this line:
 # last_three = employee_code[-3:]  # Gets the last 3 characters
 # print(last_three)
+
+
+distance_mi = 0
+is_raining = False
+has_bike = False
+has_car = False
+has_ride_share_app = False
+
+if not distance_mi:
+    print(False)
+
+elif distance_mi <= 1 and not is_raining:
+    print(True)
+
+elif distance_mi <= 6 and has_bike and not is_raining:
+    print(True)
+
+elif distance_mi > 6 and (has_car or has_ride_share_app):
+    print(True)
+
+else:
+    print(False)
