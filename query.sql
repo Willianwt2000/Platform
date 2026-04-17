c49122919a3665ddfaf46559defe69569c2a3ac7
Willianwt2000 <willianmetelien@gmail.com>
2026-04-16 23:15:49 -0400

    queries

--- query.sql
+++ query.sql
@@ -1,1 +1,14 @@
 select  * from film
+
+-- SELECT DISTINCT school
+-- FROM teachers;
+
+
+SELECT DISTINCT school,teachers.salary
+FROM teachers;
+
+
+
+SELECT first_name, last_name, salary
+FROM teachers
+ORDER BY salary DESC;
