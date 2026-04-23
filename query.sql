965fb128994a80c5ed5815ed47172186e476a28a
Willianwt2000 <willianmetelien@gmail.com>
2026-04-22 23:03:09 -0400

    testing database

--- query.sql
+++ query.sql
@@ -12,3 +12,31 @@ FROM teachers;
 SELECT first_name, last_name, salary
 FROM teachers
 ORDER BY salary DESC;
+
+
+SELECT last_name,school,hire_date 
+FROM teachers
+ORDER BY school ASC , hire_date DESC;
+
+
+SELECT last_name, school, hire_date
+FROM teachers
+WHERE school = 'Myers Middle School';
+
+
+
+
+SELECT first_name, last_name, school, salary
+FROM teachers
+WHERE salary BETWEEN 40000 AND 65000;
+SHOW ALL;
+
+SELECT column_names
+FROM table_name
+WHERE criteria
+ORDER BY column_names;
+
+
+SELECT first_name, last_name, school, hire_date, salary
+FROM teachersl
+ORDER BY hire_date DESC;
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,39 @@
-from django.test import TestCase
+import psycopg2
 
-# Create your tests here.
+try:
+    connection = psycopg2.connect(
+        database="analysis",
+        user="student",
+        password="123456",
+        host="127.0.0.1",
+        port="5432"
+    )
+
+    cursor = connection.cursor()
+
+    cursor.execute("""
+        SELECT 
+            varchar_column,
+            LENGTH(varchar_column) AS varchar_length,
+
+            char_column,
+            LENGTH(char_column) AS char_length,
+
+            text_column,
+            LENGTH(text_column) AS text_length
+        FROM char_data_types;
+    """)
+
+    records = cursor.fetchall()
+
+    for row in records:
+        print(row)
+
+except Exception as e:
+    print("Error:", e)
+
+finally:
+    if 'cursor' in locals():
+        cursor.close()
+    if 'connection' in locals():
+        connection.close()
